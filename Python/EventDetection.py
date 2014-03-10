003651d540a237064b80099144d56e351f548634
Azrrael <vcamargo.e@gmail.com>
2014-03-10 21:22:22 +0000

    Fixed wires dict

--- Python/EventDetection.py
+++ Python/EventDetection.py
@@ -2,19 +2,19 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-
+GPIO.setwarnings(False)
 
 wires = {'white':8,'grey':10,'violet':12,'blue':16,'green':18}
 
 for wire in wires.keys():
-	GPIO.setup(wires[key], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+	GPIO.setup(wires[wire], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 counter = 0
 
 while counter < 10:
 	GPIO.wait_for_edge(wires['white'], GPIO.BOTH)
 	for wire in wires.keys():
-		print 'Wire {} is {}'.format(wire,GPIO.input(wires[wire])):
+		print 'Wire {} is {}'.format(wire,GPIO.input(wires[wire]))
 	counter+=1
 
 GPIO.cleanup()
